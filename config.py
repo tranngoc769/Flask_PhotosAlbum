868a7be61159e193007e980e167de2e0e379b0f2
tranngoc769 <tnquang769@gmail.com>
2021-01-25 18:37:37 +0700

    config file

--- config.py
+++ config.py
@@ -4,3 +4,5 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 SQLALCHEMY_ECHO = False
 SQLALCHEMY_TRACK_MODIFICATIONS = True
 SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:@localhost/albumphotos"
+allow_extension = ["jpg"]
+storage_path = ""
