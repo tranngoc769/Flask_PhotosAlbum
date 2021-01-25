0e6f78c7ef93cefd8647ca8bd898de0f76aea785
tranngoc769 <tnquang769@gmail.com>
2021-01-25 18:37:00 +0700

    http code

---
+++ http_response.py
@@ -0,0 +1,39 @@
+HTTP_NotAccept = {
+'code':406,
+'msg' : "Request Not Acceptable"
+}
+
+HTTP_OK = {
+'code':200,
+'msg' : "OK"
+}
+# 
+HTTP_Accepted = {
+'code':202,
+'msg' : "Accepted"
+}
+# 
+HTTP_OK = {
+'code':200,
+'msg' : "OK"
+}
+# 
+HTTP_BadRequest = {
+'code':400,
+'msg' : "Bad Request"
+}
+# 
+HTTP_Unauthorized = {
+'code':401,
+'msg' : "Request Unauthorized"
+}
+# 
+HTTP_Forbidden = {
+'code':403,
+'msg' : "Request Forbidden"
+}
+# 
+HTTP_NotFound = {
+'code':404,
+'msg' : "Request Not Found"
+}
