5b58b487a9e5fc82bf55bc8e85110a688709cc7c
tranngoc769 <tnquang769@gmail.com>
2021-01-25 21:30:56 +0700

    album, photo model+schemas

--- Model.py
+++ Model.py
@@ -23,15 +23,17 @@ class Album(db.Model):
 class Photo(db.Model):
     __tablename__ = 'photo'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    name = db.Column(db.String(200), unique=True, nullable=False)
+    name = db.Column(db.String(200), nullable=False)
+    save_name = db.Column(db.String(200), unique=True, nullable=False)
     id_album = db.Column(db.Integer, db.ForeignKey('album.id'), nullable=False)
     description = db.Column(db.String(1000), nullable=True)
     path = db.Column(db.String(1000), nullable=False)
     date_created = db.Column(db.DateTime, nullable= False)
-    def __init__(self, name, id_album, description, path, date_created):
+    def __init__(self, name, id_album, description, path, date_created,save_name):
         self.name = name
         self.id_album = id_album
         self.description = description
+        self.save_name = save_name
         self.path = path
         self.date_created = date_created
 
@@ -45,6 +47,7 @@ class PhotoSchema(ma.Schema):
     id = fields.Integer(dump_only=True)
     id_album = fields.Integer(required=True)
     name = fields.String(required=True)
+    save_name = fields.String(required=True)
     path = fields.String(required=True)
     description = fields.String(required=True)
     date_created = fields.DateTime(required=True)
