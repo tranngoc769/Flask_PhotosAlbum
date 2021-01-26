47150691594af6dc30676e6e85d16d466478cf73
tranngoc769 <tnquang769@gmail.com>
2021-01-26 11:19:54 +0700

    User Model + Schema

--- Model.py
+++ Model.py
@@ -19,7 +19,22 @@ class Album(db.Model):
         self.date_created = date_created
         self.description = description
 
-
+class User(db.Model):
+    __tablename__ = 'user'
+    id = db.Column(db.Integer, primary_key=True,autoincrement=True)
+    username = db.Column(db.String(50), nullable=False)
+    fullname = db.Column(db.String(100), nullable=False)
+    password = db.Column(db.String(1000), nullable=False)
+    email = db.Column(db.String(100), nullable=True)
+    date_created = db.Column(db.DateTime, nullable= False)
+    role = db.Column(db.Integer,nullable=False, default=2)
+    def __init__(self, username, fullname, password,email,date_created, role):
+        self.username = username
+        self.fullname = fullname
+        self.password = password
+        self.email = email
+        self.date_created = date_created
+        self.role = role
 class Photo(db.Model):
     __tablename__ = 'photo'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
@@ -36,8 +51,6 @@ class Photo(db.Model):
         self.save_name = save_name
         self.path = path
         self.date_created = date_created
-
-
 class AlbumSchema(ma.Schema):
     id = fields.Integer(dump_only=True)
     name = fields.String(required=True)
@@ -51,3 +64,11 @@ class PhotoSchema(ma.Schema):
     path = fields.String(required=True)
     description = fields.String(required=True)
     date_created = fields.DateTime(required=True)
+class UserSchema(ma.Schema):
+    id = fields.Integer(dump_only=True)
+    username = fields.Integer(required=True)
+    fullname = fields.String(required=True)
+    password = fields.String(required=True)
+    email = fields.Email(required=True)
+    date_created = fields.DateTime(required=True)
+    role = fields.Integer(required=True)
